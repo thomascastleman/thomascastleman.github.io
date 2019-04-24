03fb71c33c59dec425651a53f4118285ba8abe1d
Thomas Castleman <thomas7rex@gmail.com>
2019-04-23 22:22:45 -0400

    setup basic node app

---
+++ server.js
@@ -0,0 +1,19 @@
+
+var express 			= require('express');
+var app 				= express();
+var mustacheExpress 	= require('mustache-express');
+var bodyParser 			= require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine('html', mustacheExpress());
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+var sys = require('./settings.js');
+
+// start server
+var server = app.listen(sys.PORT, function() {
+	console.log('tcastleman.com server listening on port %d', server.address().port);
+});
---
+++ settings.js
@@ -0,0 +1,9 @@
+
+/*
+	settings.js: System constants and settings
+*/
+
+module.exports = {
+	// server port
+	PORT: 8080
+}
