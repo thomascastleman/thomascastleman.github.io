7d8702a0e67eacb4cd243d92c9620efad93a4cdb
Thomas Castleman <thomas7rex@gmail.com>
2020-09-08 14:57:48 -0400

    add L-systems update as git submodule

--- views/Lsystems/globals.js
+++
@@ -1,8 +0,0 @@
-/*
-  globals.js: Global constants & variables
-*/
-
-const STEP_LENGTH = 15;   // length of turtle graphics "forward" movements
-
-let lsys;       // the L-system itself
-let lineColor;  // color of turtle graphics lines
